4a89bf643debe6ae3c4ba7f3cef6b33a49b9370c
Calum Armstrong <calum.armstrong.2020@uni.strath.ac.uk>
2025-03-12 18:49:53 +0000

    Completed filter function

--- MiniProject/Main.c
+++ MiniProject/Main.c
@@ -1,9 +1,19 @@
-#include "Main.h"
 #include "Maincfg.h"
 #include "framework.h"
+#include "IIR_coeffs/bp.h"
+#include "IIR_coeffs/lp.h"
+#include "IIR_coeffs/hp.h"
+#include "Main.h"
 
 uint32_t dip_all;
-uint8_t dip_1, dip_2, dip_3, dip_4, dip_5, dip_6, dip_7, dip_8;
+uint8_t dip_1, dip_2, dip_6, dip_7, dip_8;
+
+float buffer[32000];
+int ind = 0;
+float x[N_IIR_BP] = {0};
+float yb[N_IIR_BP] = {0};
+float yl[N_IIR_LP] = {0};
+float yh[N_IIR_HP] = {0};
 
 void main(void){
     initAll();
@@ -24,21 +34,22 @@ void audioHWI(void){
     //printf("%d\n",s16);
 
     //printf("test2\n");
+
     DIP_get(DIP_1,&dip_1);
     if(dip_1){
 
         DIP_get(DIP_2,&dip_2);
         if(dip_2){
-            //filters function
-            //output result
+            filters();
             printf("filter\n");
         }
         else{
-            //buffer store function
+            buffer[ind] = (float)s16;
             //output input?
             //run IIR with zeros?
             printf("buffer\n");
         }
+        ind = (ind+1)%32000;
     }
     else{
         //do nothing
@@ -46,46 +57,56 @@ void audioHWI(void){
         //run IIR with zeros?
         //printf("nothing\n");
     }
-    write_audio_sample(s16);
 }
 
 void LED(){
     printf("test\n");
 }
 
-void buffer(){
-    //int16_t s16;
-    //s16 = read_audio_sample();
-}
-
 void filters(){
-    //update x with new value
+    update_array(x,buffer[ind]);
 
-    //if 2.6
-    //call IIR with LP filter
-    //update y with new value
+    float result = 0.0;
+    DIP_get(DIP_6,&dip_6);
+    if(dip_6){
+        result += IIR(x,b_iir_bp,yb,a_iir_bp);
+    }
 
-    //if 2.6
-    //call IIR with BP filter
-    //update y with new value
+    DIP_get(DIP_7,&dip_7);
+    if(dip_7){
+        result += IIR(x,b_iir_lp,yl,a_iir_lp);
+    }
 
-    //if 2.6
-    //call IIR with HP filter
-    //update y with new value
+    DIP_get(DIP_8,&dip_8);
+    if(dip_8){
+        result += IIR(x,b_iir_hp,yh,a_iir_hp);
+    }
 
     //sum filter results
-    //output result
+    write_audio_sample(result);
 }
 
+
+//TODO unroll
 float IIR(float x[],float b1[], float y[],float b2[]){
     int i;
     float out = 0.0;
-    for(i = 0; i<100; i++){
+    for(i = 0; i<N_IIR_BP; i++){
         out += x[i]*b1[i];
     }
-    for(i = 0;i<100; i++){
+    for(i = 0;i<N_IIR_BP; i++){
         out += y[i]*b2[i];
     }
+    update_array(y,out);
     return out;
 }
+
+void update_array(float arr[], float new){
+    int i = 0;
+    for(i = N_IIR_LP; i>1; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = new;
+}
+
 
--- MiniProject/Main.h
+++ MiniProject/Main.h
@@ -1,4 +1,4 @@
 float IIR(float x[],float b1[], float y[],float b2[]);
-void buffer();
 void filters();
 void LED();
+void update_array(float arr[], float new);
