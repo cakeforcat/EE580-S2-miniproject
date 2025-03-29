e01c5ffeaaf07f1cf5d52b78149ce9dc645ec75b
Calum Armstrong <calum.armstrong.2020@uni.strath.ac.uk>
2025-03-29 23:54:27 +0000

    flip coefficients back

--- MiniProject/Main.c
+++ MiniProject/Main.c
@@ -99,11 +99,11 @@ void audioHWI(void){
                 //add outputs from IIR filters to output
                 //filters used based on switch states
                 //switch S2:7
-                if(!dips[6]){
+                if(!dips[5]){
                     result += IIR(a_iir_lp,yb,b_iir_lp);
                 }
                 //switch S2:6
-                if(!dips[5]){
+                if(!dips[6]){
                     result += IIR(a_iir_bp,yl,b_iir_bp);
                 }
                 //switch S2:8
